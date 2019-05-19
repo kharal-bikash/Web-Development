95594ba49576f7bf81d2502a0efa922d6c1e45eb
kharal-bikash <kharal-bikash@users.noreply.github.com>
2019-05-19 19:37:34 -0400

    Add files via upload

---
+++ web-development/Part 1/test-site/scripts/main.js
@@ -0,0 +1,59 @@
+var myVariable = 'Bikash'; // Declaring the variable.
+
+myVariable = 'Kharal'; // Changing the value of myVariable to Kharal.
+
+/*
+
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+*/
+
+
+var myImage = document.querySelector('img');
+var button = document.getElementById('myImage');
+button.addEventListener("click", myImage, false);
+
+myImage.onclick = function() {
+
+  var mySrc = myImage.getAttribute('src');
+
+  if (mySrc === 'images/Bikash.jpg') {
+
+    myImage.setAttribute('src', 'images/Bikash2.jpg');
+
+  } else {
+
+    myImage.setAttribute('src', 'images/Bikash3.jpg');
+
+  }
+};
+
+var myButton = document.querySelector('button1');
+
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Bikash is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Bikash is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
